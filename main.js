566501939f70e39454a3cc08aeca1ff36560c6c7
Arshaq123 <75874459+Arshaq123@users.noreply.github.com>
2022-10-07 14:16:31 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,36 @@
+prediction = "";
+
+Webcam.set(
+    {
+        width:375,
+        height:499,
+        image_format:'png',
+        png_quality:90
+    }
+);
+
+camera = document.getElementById("camera");
+Webcam.attach(
+    '#camera'
+);
+
+function take_Snapshot(){
+    Webcam.snap(function(data_uri)
+    {
+document.getElementById("result").innerHTML ='<img id="captured_image" src="'+data_uri+'">';
+    });
+}
+
+console.log("ml5 version:",ml5.version);
+
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/Qorn6llzx/model.json',modelLoaded);
+
+function modelLoaded(){
+    console.log("model_loaded");
+}
+function speak() {
+    var synth = window.speechSynthesis;
+    var speak_data = "The Prediction Is "+prediction;
+    var utterThis = new SpeechSynthesisUtterance(speak_data);
+    synth.speak(utterThis);
+}
